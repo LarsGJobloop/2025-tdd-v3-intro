790d735c1cf964fe795d2a3cfb7d3b7f4ce01dbc
zabronax <zabronax@gmail.com>
2025-05-19 11:05:55 +0200

    setup failing test

--- Calculator.Tests/Calculator.cs
+++ Calculator.Tests/Calculator.cs
@@ -1,5 +1,20 @@
 ﻿namespace Calculator.Tests;
 
-public class Calculator
+public class Add
 {
+    [Fact]
+    public void TwoAndTwoShouldResultInFour()
+    {
+        // Arrange
+        var calculator = new Calculator();
+        int a = 2;
+        int b = 2;
+        int expected = 4;
+
+        // Act
+        var result = calculator.Add(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
 }
